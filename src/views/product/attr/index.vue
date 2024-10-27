5bdf6b9f067ea01457c8b029e4a3fd4df0115d23
Bing <wangbing@hcjian.cn>
2024-10-27 21:28:07 +0800

    fix:细节修复

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -4,8 +4,6 @@
     <el-card style="margin-top: 10px;">
         <div v-show="scene == 0">
             <el-button type="primary" icon="Plus" :disabled="!categoryStore.c3Id" @click="addAttr">添加平台属性</el-button>
-            <el-button type="primary" icon="Plus"  @click="open2">添加平台属性</el-button>
-
             <el-table border style="margin: 10px 0px;" :data="attrList">
 
                 <el-table-column label="序号" type="index" width="80px" />
@@ -19,8 +17,8 @@
                 </el-table-column>
                 <el-table-column label="操作" width="120px">
                     <template #="{ row, $index }">
-                        <el-button type="primary" icon="Edit" size="small" @click="updateAttr"></el-button>
-                        <el-popconfirm :title="`确定要删除${row.valueName}嘛?`" icon="Delete" @confirm="deleteAttr(row.id)" width="260px">
+                        <el-button type="primary" icon="Edit" size="small" @click="updateAttr(row,$index)"></el-button>
+                        <el-popconfirm :title="`确定要删除${row.attrName}嘛?`" icon="Delete" @confirm="deleteAttr(row.id)" width="260px">
                             <template #reference>
                                 <el-button type="primary" size="small" icon="Delete"></el-button>
                             </template>
@@ -47,26 +45,33 @@
                 <el-table-column label="序号" type="index" width="80" align="center" />
                 <el-table-column label="属性值" align="center">
                     <template #default="{ row, $index }">
-                        <el-input :ref="$index" v-model="row.valueName" placeholder="请输入属性值" />
+                        <el-input v-if="row.flag" :ref="(vc:any)=>inputArr[$index]=vc"  v-model="row.valueName" @blur="toBlur(row,$index)" autofocus placeholder="请输入属性值" />
+                        <div v-else style="text-align: left;" @click="toEdit(row,$index)">{{ row.valueName }}</div>
                     </template>
                 </el-table-column>
-                <el-table-column label="操作" width="140" align="center" />
+                <el-table-column label="操作" width="140" align="center" >
+                    <template #default="{row,$index}">
+                        <el-button type="danger" icon="Delete" size="mini" @click="attrParams.attrValueList.splice($index,1)"></el-button>
+                    </template>
+                </el-table-column>
             </el-table>
-            <el-button type="primary" @click="saveAttr">保存</el-button>
+            <el-button type="primary" @click="saveAttr" :disabled="attrParams.attrValueList.length>0?false:true">保存</el-button>
             <el-button type="primary" @click="cancelAttrAdd">取消</el-button>
         </div>
     </el-card>
 </template>
 <script setup lang="ts">
-import { reactive, ref, watch } from 'vue'
+import { nextTick, reactive, ref, watch } from 'vue'
 import useCategoryStore from '@/store/modules/product';
 import { reqAddOrUpdateAttr, reqAllAttrInfo,reqDeleteAttr } from '@/api/product/attr';
-import { Attr } from '@/api/product/attr/type';
+import { Attr, AttrValue } from '@/api/product/attr/type';
 
 const attrList = ref<Attr[]>([]);
 
 const categoryStore = useCategoryStore()
 
+
+let inputArr = ref([]);
 
 const scene = ref<number>(0)
 
@@ -84,6 +89,9 @@ const addAttrValue = () => {
         valueName: '',
         flag: true
     });
+    nextTick(() => {
+        inputArr.value[attrParams.attrValueList.length - 1].focus();
+        });
 };
 
 watch(() => categoryStore.c3Id, async () => {
@@ -94,8 +102,14 @@ watch(() => categoryStore.c3Id, async () => {
 
 })
 
-const updateAttr = () => {
+const updateAttr = (row:any,$index:number) => {
+    //浅拷贝
+    //Object.assign(attrParams,row);
+
+    //深拷贝
+    Object.assign(attrParams,JSON.parse(JSON.stringify(row)))
     scene.value = 1;
+    
 }
 
 const getAttrList = async (c1Id: number | string, c2Id: number | string, c3Id: number | string) => {
@@ -108,6 +122,12 @@ const getAttrList = async (c1Id: number | string, c2Id: number | string, c3Id: n
 
 }
 const addAttr = () => {
+    Object.assign(attrParams, {
+        attrName: '',
+        categoryId: '',
+        categoryLevel: 3,
+        attrValueList: []
+    });
     scene.value = 1;
     attrParams.categoryId = categoryStore.c3Id;
 }
@@ -116,6 +136,27 @@ const cancelAttrAdd = () => {
     scene.value = 0;
 }
 
+const toBlur = (row: AttrValue,$index:number) => {
+    if (row.valueName.trim().length === 0) {
+        attrParams.attrValueList.splice($index,1)
+        ElMessage.error('属性值不能为空');
+        return;
+    }
+    if(attrParams.attrValueList.find(item=>item!=row&&item.valueName===row.valueName)){
+        attrParams.attrValueList.splice($index,1)
+        ElMessage.error('属性值重复');
+        return;
+    }
+    row.flag=false
+}
+
+const toEdit = (row:AttrValue,index:number) => {
+    row.flag=true
+    // row.edit=true
+    nextTick(()=>{
+        inputArr.value[index].focus()
+    })
+}
 
 const deleteAttr = async (id: number) => {
     attrParams.id = id;
@@ -131,23 +172,6 @@ const deleteAttr = async (id: number) => {
         ElMessage({ type: 'fail', message: '删除失败' });
      }
 }
-
-const saveAttr2 = async () => {
-    let response = await reqAddOrUpdateAttr(attrParams);
-    console.log(response)
-    if (response.code === 200) {
-        scene.value = 0;
-        ElMessage({
-            type: 'success',
-            message: '删除成功',
-        })
-        ElMessage.success(attrParams.id ? "修改成功" : "添加成功");
-        getAttrList(categoryStore.c1Id, categoryStore.c2Id, categoryStore.c3Id);
-    } else {
-        ElMessage.fail(attrParams.id ? "修改失败" : "修改成功");
-    }
-}
-
 
 const saveAttr = async () => {
   let res: any = await reqAddOrUpdateAttr(attrParams)
