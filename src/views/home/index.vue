92a8dc6f662f7cea29b14ffc6824b3195bf5edd3
Bing <wangbing@hcjian.cn>
2024-10-29 20:54:25 +0800

    fix:首页数据展示

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,53 @@
-
 <template>
-   <el-button :plain="true" @click="open2">Success</el-button>
+  <el-card>
+    <div class="box">
+      <img :src="userStore.avatar" alt="" class="avatar" />
+      <div class="bottom">
+        <h3 class="title">{{ getTime() }}好， {{ userStore.username }}</h3>
 
+      </div>
+    </div>
+  </el-card>
+  <div class="bottom2">
+
+    <DefSvgIcon name="welcome" width="500px" height="500px"></DefSvgIcon>
+  </div>
 </template>
 
 
 <script setup lang="ts">
- 
-const open2 = () => {
-  ElMessage({
-    message: 'Congrats, this is a success message.',
-    type: 'success',
-  })
-}
+import { DefSvgIcon } from '@/components';
+import useUserStore from '@/store/modules/user';
+import { getTime } from '@/utils/time';
+
+
+
+let userStore = useUserStore();
+
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box {
+  display: flex;
+
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin-right: 20px;
+  }
+  .title{
+
+  }
+  .bottom{
+    margin-left: 20px;
+    .title{
+      font-size: 30px;
+      font-weight: 900;
+    }
+  }
+}
+.bottom2{
+  display: flex;
+  justify-content: center;
+}
+</style>
